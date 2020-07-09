969152fa00fa66cba5291dd55503367cfb17cc6b
ivory627 <58761162+ivory627@users.noreply.github.com>
2020-07-09 16:34:58 +0900

    programmers coding test

---
+++ src/PandYCount.java
@@ -0,0 +1,30 @@
+
+public class PandYCount {
+
+	public static void main(String[] args) {
+		System.out.println(solution("ppooyY"));
+	}
+	
+	public static boolean solution(String s) {
+		boolean answer = true;
+		int pCnt = 0;
+		int yCnt = 0;
+		
+		for(int i=0; i<s.length(); i++) {
+			if(s.charAt(i)=='p' || s.charAt(i)=='P') {
+				pCnt++;
+			}
+			
+			if(s.charAt(i)=='y' || s.charAt(i)=='Y') {
+				yCnt++;
+			}
+		}
+		
+		if(pCnt!=yCnt) {
+			return false;
+		}
+		
+		return answer;
+	}
+
+}
