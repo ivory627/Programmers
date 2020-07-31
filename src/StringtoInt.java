e13224ba7524428d29d9e873587b76afb62db177
ivory627 <58761162+ivory627@users.noreply.github.com>
2020-07-31 16:31:03 +0900

    string to int, subak

---
+++ src/StringtoInt.java
@@ -0,0 +1,14 @@
+
+public class StringtoInt {
+
+	public static void main(String[] args) {
+		System.out.println(solution("+1234"));
+	}
+	
+	public static int solution(String s) {
+		int answer = 0;
+		answer = Integer.parseInt(s);
+		return answer;
+	}
+
+}
