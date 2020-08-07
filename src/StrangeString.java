ad07e3187df5dac7868f6d811074e164a5cf850f
ivory627 <58761162+ivory627@users.noreply.github.com>
2020-08-07 16:35:41 +0900

    이상한 문자 만들기

---
+++ src/StrangeString.java
@@ -0,0 +1,31 @@
+import java.util.StringTokenizer;
+
+public class StrangeString {
+
+	public static void main(String[] args) {
+		System.out.println(solution("try hello world"));
+	}
+	
+	public static String solution(String s) {
+        String answer = "";
+        StringTokenizer st = new StringTokenizer(s," ",true);
+        
+        while(st.hasMoreTokens()) {
+        	String str = st.nextToken();
+        	if(str.equals(" ")) {
+        		answer += str;
+        		continue;
+        	}
+        	for(int i=0; i<str.length(); i++) {
+        		if(i%2==0) {
+        			answer += Character.toUpperCase(str.charAt(i));
+        		}else {
+        			answer += Character.toLowerCase(str.charAt(i));
+        		}
+        	}
+        }
+        
+        return answer;
+    }
+
+}
