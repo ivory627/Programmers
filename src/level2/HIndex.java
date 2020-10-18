5e4c5536a2f83f4c48f5efb22d41ac4a36ec0784
ivory <ivory627@naver.com>
2020-10-18 15:26:00 +0900

    Programmers h-index 부연설명 추가

--- src/level2/HIndex.java
+++ src/level2/HIndex.java
@@ -11,9 +11,11 @@ public class HIndex {
 	
 	public static int solution(int[] citations) {
         int answer = 0;
+        //h번 이상 인용된 논문이 h편 이상이고 나머지가 h번 이하인 h의 최댓값을 구하므로
+        //정렬 후 앞에서부터 검사하여 처음 h-index가 성립한 순간을 리턴
         Arrays.sort(citations);
-        
         for(int i=0; i<citations.length; i++) {
+        	//h를 줄여나가며 검사
         	int h = citations.length-i;
         	if(citations[i]>=h) {
         		answer = h;
