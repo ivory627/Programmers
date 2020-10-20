ff0a7fc0670978ac3a24f9baedc0b0054e1f7169
ivory <ivory627@naver.com>
2020-10-20 14:26:51 +0900

    Programmers 더 맵게 부연설명 추가

--- src/level2/MoreSpicier.java
+++ src/level2/MoreSpicier.java
@@ -12,13 +12,16 @@ public class MoreSpicier {
 
 	public static int solution(int[] scoville, int K) {
 		int answer = 0;
+		//우선순위큐 생성 -> heap자료구조 형태로 저장
+		//우선순위가 높은 순으로 정렬
 		PriorityQueue<Integer> pq = new PriorityQueue<Integer>();
 		for (int i : scoville) {
 			pq.add(i);
 		}
-		
+			//첫번째 값(최소 값)이 스코빌지수를 넘을때까지 반복
 			while (pq.peek() <= K) {
-				if (pq.peek() == 0 || pq.size()<2) return -1;
+				//우선순위큐에 값이 2개 미만이면 스코빌지수를 만들수없으므로 -1리턴
+				if (pq.size()<2) return -1;
 				int num = 0;
 				num = pq.poll() + pq.poll() * 2;
 				pq.add(num);
