129b6905a98f53e3d25fdc6986b8ec03f2be39a4
psa0627 <psa0627@DESKTOP-19QBJ3K>
2020-10-29 11:03:49 +0900

    Programmers 카펫 오타수정

--- src/level2/Carpet.java
+++ src/level2/Carpet.java
@@ -11,7 +11,7 @@ public class Carpet {
 	//카펫 둘레(brown) 구하기 : 2x + 2y -4(모서리 겹치는 부분)
 	//식을 정리하면  x + y = (brown + 4) / 2
 	//카펫 안쪽 넓이(yellow) 구하기 : (x-2)*(y-2) -> 가로,세로 각각 겹치는 부분빼서 곱하기
-	//조건중 yellow는 1이상이기 때문에 x>=3, 가로 세로가 같거나 가로가 더 크므로 x>=y
+	//조건중 yellow는 1이상이기 때문에 y>=3, 가로 세로가 같거나 가로가 더 크므로 x>=y
 	public static int[] solution(int brown, int yellow) {
         int[] answer = new int[2];
         int x, y = 0;
