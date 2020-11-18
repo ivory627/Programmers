1a64198c41f38e056e9915e763fc7fefb0842ff8
ivory627 <ivory627@naver.com>
2020-11-18 10:47:29 +0900

    Programmers 내적

---
+++ src/level1/DotProduct.java
@@ -0,0 +1,24 @@
+package level1;
+
+public class DotProduct {
+	
+	public static void main(String[] args) {
+		int[] a = {1,2,3,4};
+		int[] b = {-3,-1,0,2};
+		
+		solution(a,b);
+		
+	}
+	
+	public static int solution(int[] a, int[] b) {
+        int answer = 0;
+        
+        for(int i=0; i<a.length; i++) {
+        	answer += a[i]*b[i];
+        }
+        
+        System.out.println(answer);
+        
+        return answer;
+    }
+}
